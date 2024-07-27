3ff952b72d31f3df04cd25e1cf7e779c5ea244e8
Ivan <vanechka-nikitin-2004@mail.ru>
2024-07-28 01:36:09 +0800

    Update serializers

--- project/ads/serializers.py
+++ project/ads/serializers.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.models import User
+
 from rest_framework import serializers
 
 from .models import Ad
@@ -7,3 +9,14 @@ class AdSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ad
         fields = ['id', 'title', 'ad_id', 'author', 'views_count', 'position']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
